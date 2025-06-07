e534884c82c74f79eda9daf2c0c6b9ec80db3939
Seunghyub choi <seunghyub.choi@gmail.com>
2025-06-07 10:04:31 +0900

    단위 테스트 3 - DisplayName, BDD(g,w,t)

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.unit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverages.Americano;
 import sample.cafekiosk.unit.beverages.Latte;
@@ -20,6 +21,8 @@ class CafeKioskTest {
         System.out.println(">>> 음료 : " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+    //@DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.") // 행위 -> 결과 명시하기
     @Test
     void add() {
          CafeKiosk cafeKiosk = new CafeKiosk();
@@ -82,19 +85,33 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
-
         Americano americano = new Americano();
         Latte latte = new Latte();
 
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
 
+        // then
         assertThat(totalPrice).isEqualTo(8500);
+
+    }
+
+    @DisplayName("")
+    @Test
+    void test() {
+        // given
+
+        // when
+
+        // then
 
     }
 
